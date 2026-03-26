631ed6b7e854fe7c399fd1a926739193fb5af870
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-26 10:19:28 -0500

    dict_func.py

--- dict_func.py
+++ dict_func.py
@@ -2,9 +2,9 @@
 employee_info = {"Name": "Juan",
                  "Salary": 30,
                  "Role": "Developer"}
-def employee_print():
+def employee_print(employee_info):
     for key, value in employee_info.items():
         print(f"{key}: {value}")
     after_print = "No other info!"
     print(after_print)
-employee_print()
+employee_print(employee_info)
