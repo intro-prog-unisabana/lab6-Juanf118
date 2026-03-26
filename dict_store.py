91245a8b18d71244ea3d25df62366c6df4274b97
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-25 20:55:05 -0500

    dict_store.py

--- dict_store.py
+++ dict_store.py
@@ -1,1 +1,14 @@
 # Write your code here!
+
+
+data = {"temp": 25,
+        "color": "blue"
+        }
+def temp_and_color(values):
+ #temp = print (data.get("temp"))
+ #color = print (data.get("color"))
+
+
+ return data.get("temp"), data.get("color")
+
+print(temp_and_color(data.values()))
