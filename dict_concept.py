0cca7b8f906e3bd9974e0b11f828a8b2918460e9
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-25 19:18:02 -0500

    dict_concept.py

--- dict_concept.py
+++ dict_concept.py
@@ -1,8 +1,8 @@
 # Write your code here!
 get_concept = {"width": 480, 
-                 "height": 480,
+                "height": 480,
                 "color_mode": "dark",
-                  "sensivity": 0.4}
+                "sensitivity": 0.4}
 def get_config():
     return get_concept
 print(get_config())
