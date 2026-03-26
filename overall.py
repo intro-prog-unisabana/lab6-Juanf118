2a9f2759475d6886ee666b1d335e967ecd203aa1
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-26 10:49:00 -0500

    overall.py

--- overall.py
+++ overall.py
@@ -1,13 +1,12 @@
 my_dict = {
     "student_id1":{"assignment1":100, "assignment2":90, "assignment3":80},
-    "student_id2":{"assignment1":90, "assignment2":80, "assignment3":70},
-    "student_id3":{"assignment1":80, "assignment2":70, "assignment3":60}
-}
+    "student_id2":{"assignment1":60, "assignment2":80, "assignment3":70},
+    "student_id3":{"assignment1":80, "assignment2":90, "assignment3":60}}
 def student_averages(my_dict):
     averages = {}
     for student_id, assignments in my_dict.items():
         average = sum(assignments.values()) / len(assignments)
-        averages[student_id] = average
+        averages[student_id] = int(average)
     return averages
 print(student_averages(my_dict))
 
@@ -21,6 +20,6 @@ def assignments_average(my_dict):
                 assignment_prom[assignment] = 0
             assignment_totals[assignment] += score
             assignment_prom[assignment] += 1
-    averages = {assignment: assignment_totals[assignment] / assignment_prom[assignment] for assignment in assignment_totals}
+    averages = {assignment: int(assignment_totals[assignment] / assignment_prom[assignment]) for assignment in assignment_totals}
     return averages
 print(assignments_average(my_dict))
