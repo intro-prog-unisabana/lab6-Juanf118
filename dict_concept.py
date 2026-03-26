183b9adb8e7afd48373cc0f0caa7c01a5d533d41
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-25 19:13:30 -0500

    dict_concept.py

--- dict_concept.py
+++ dict_concept.py
@@ -3,6 +3,6 @@ dict_concepto = {"width": 480,
                  "height": 480,
                 "color_mode": "dark",
                   "sensivity": 0.4}
-def get_concept(dict_concepto):
+def get_concept():
     return dict_concepto
-print(get_concept(dict_concepto))
+print(get_concept())
