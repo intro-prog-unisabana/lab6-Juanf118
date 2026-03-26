2492ebd4819e894040b1988ca17fc81e88812f8b
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-25 19:09:06 -0500

    dict_concept.py

--- dict_concept.py
+++ dict_concept.py
@@ -1,5 +1,8 @@
 # Write your code here!
-dict_concepto = {"width": 480, "height": 480, "color_mode": "dark", "sensivity": 0.4}
+dict_concepto = {"width": 480, 
+                 "height": 480,
+                "color_mode": "dark",
+                  "sensivity": 0.4}
 def get_concept(dict_concepto):
     return dict_concepto
 print(get_concept(dict_concepto))
