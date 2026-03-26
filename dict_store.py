fd807543b4ff7772c8f80d3c74c37be2a959ed12
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-25 20:58:27 -0500

    dict_store.py

--- dict_store.py
+++ dict_store.py
@@ -1,14 +1,14 @@
 # Write your code here!
 
 
-data = {"temp": 25,
-        "color": "blue"
+data = {"temp": 22.5,
+        "color": "blue",
+        "status": "ok"
         }
 def temp_and_color(values):
- #temp = print (data.get("temp"))
- #color = print (data.get("color"))
+    if "temp" not in data and "color" not in data:
+          return None
 
-
- return data.get("temp"), data.get("color")
+    return data.get("temp"), data.get("color")
 
 print(temp_and_color(data.values()))
