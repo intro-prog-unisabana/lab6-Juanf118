1094dd45b00d619985b904e0acf71ee78a7294d4
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-25 21:47:38 -0500

    dict_store.py

--- dict_store.py
+++ dict_store.py
@@ -5,10 +5,16 @@ data = {"temp": 22.5,
         "color": "blue",
         }
 def temp_and_color(values):
-    if "temp" in data and "color" in data:
-
-        return data.get("temp"), data.get("color")
-    if "temp" not in data and "color" not in data:
+    if not isinstance(values, dict):
         return None, None
-
+    if "color" not in values or "temp" not in values:
+        return None, None
+    return values.get("temp"), values.get("color")
 print(temp_and_color(data))
+   # if "temp" in data and "color" in data:
+
+    #    return data.get("temp"), data.get("color")
+    #if "temp" not in data and "color" not in data:
+     #   return None, None
+
+#print(temp_and_color(data))
