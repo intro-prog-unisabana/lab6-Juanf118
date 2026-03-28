e0fe59da2d39a1b0a994ecd17774c09002db5837
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-28 12:53:14 -0500

    dict_func.py

--- dict_func.py
+++ dict_func.py
@@ -1,12 +1,11 @@
 # Write your code here!
 employee_info = {"Name": "Juan",
                  "Salary": 30000,
-                 "Role": "Developer"}
+                 "Role": "Developer",
+                 "Years at Company": 5,
+                 "Department": "IT"}
 def employee_print(employee_info):
     for key, value in employee_info.items():
         print(f"{key}: {value}")
-    valores_temp = employee_info.pop("Name, Salary, Role")
-    after_print = "No other info!"
-    employee_info
-    print(after_print)
+    print("No other info!")
 employee_print(employee_info)
